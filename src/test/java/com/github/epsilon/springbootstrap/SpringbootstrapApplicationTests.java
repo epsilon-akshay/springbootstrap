0bca9f057437e4cae7c781a97ff16dcc3822514e
akshay nambiar <epsilon@akshays-MacBook-Air.local>
2023-07-24 11:48:40 +0530

    inititalised project

---
+++ src/test/java/com/github/epsilon/springbootstrap/SpringbootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.epsilon.springbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
