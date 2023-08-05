bab36a1a0b831fb72a88594f15357ad7a253e08d
Akshay Nambiar <akshaychaos@gmail.com>
2023-08-05 15:44:43 +0530

    add example for value injection

--- src/main/java/com/github/epsilon/springbootstrap/service/BasicGreeting.java
+++ src/main/java/com/github/epsilon/springbootstrap/service/BasicGreeting.java
@@ -1,12 +1,23 @@
 package com.github.epsilon.springbootstrap.service;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.PSource;
 
 @Service
 public class BasicGreeting implements Greeting {
+
+    private final String val ;
+
+    public BasicGreeting(@Value("helllllll") String val) {
+        this.val = val;
+    }
+
+
     @Override
     public String sayHello() {
+        System.out.println(val);
         return "basic hello";
     }
 }
